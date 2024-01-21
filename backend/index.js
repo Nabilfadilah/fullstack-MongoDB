937dc82ab9c9ccaf63772fe9c30ee249c0ec1422
Nabilfadilah <mohammadfadilah00@gmail.com>
2024-01-21 15:32:11 +0700

    add file index for conection mongoDB and server

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,27 @@
 import express from "express"
 import mongoose from "mongoose"
 import cors from "cors"
+import UserRoute from "./routes/UserRoute.js"
 
 const app = express();
+
+// koneksi
+// mongoose.connect('mongodb://localhost:27017/fullstack_db', {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+// });
+
+// mongoose.connect('mongodb://localhost:27017/fullstack_db');
+
+// solusi yang eklesif 
+mongoose.connect('mongodb://127.0.0.1:27017/fullstack_db');
+
+const db = mongoose.connection;
+db.on('error', (error) => console.log(error));
+db.once('open', () => console.log('Database Connected...'));
+
+app.use(cors());
+app.use(express.json());
+app.use(UserRoute);
 
 app.listen(5000, ()=> console.log('server up and running...'))
